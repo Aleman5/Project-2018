b37de1b1bb22d7da13fb335ea9476039297f0096
Unknown <ale.bros@hotmail.com>
2018-01-18 22:10:36 -0300

    First Things Added

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+
+	public BoxCollider2D boxCollider2D;
+	public Rigidbody2D rigidBody2D;
+	public int hp;
+	public float speed;
+	public float weight;
+	public float shield;
+	//public float gravity; // Don't know how to implement it.
+
+	protected int floorMask;
+	protected Vector3 movement;
+	protected Animator animator;
+
+	protected virtual void Start () 
+	{
+		boxCollider2D = GetComponent <BoxCollider2D> ();
+		rigidBody2D = GetComponent <Rigidbody2D> ();
+		hp = 100;
+		speed = 4f;
+		weight = 0f;
+		shield = 0f;
+		//gravity = 9.8f;
+
+		floorMask = LayerMask.GetMask ("Floor");
+		movement.Set (0, 0, 0);
+		animator = GetComponent<Animator> ();
+	}
+	/*
+	protected virtual void OnCollide <T> ()
+	{
+		
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	*/
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject {
+
+	private enum States
+	{	
+		Idle,
+		Moving,
+		Jumping,
+		Died
+	};
+
+	private States currentState;
+
+	public Vector2 jumpingForce;
+
+	protected override void Start () 
+	{
+		currentState = States.Idle;
+		jumpingForce.Set (0, 20f);
+		//rigidBody2D.gravityScale = gravity;
+
+		base.Start ();
+	}
+	private void FixedUpdate () // It's using "FixedUpdate" because a Rigidbody is being used.
+	{
+		StateMachine ();
+	}
+	private void StateMachine()
+	{
+		switch (currentState) 
+		{
+		case States.Idle:
+			Movement ();
+			Jump ();
+			break;
+		case States.Moving:
+			Movement ();
+			Jump ();
+			break;
+		case States.Jumping:
+			Movement ();
+			break;
+		case States.Died:
+			break;
+		}
+	}
+	private void Movement()
+	{
+		movement.Set (0, 0, 0);
+		if (Input.GetButton ("Right"))
+			movement.Set (Input.GetAxisRaw("Right"), 0, 0);
+		else if(Input.GetButton("Left"))
+			movement.Set (-Input.GetAxisRaw("Left"), 0, 0);
+		movement = movement.normalized * speed * Time.deltaTime;
+		rigidBody2D.MovePosition (transform.position + movement);
+	}
+	private void Jump()
+	{
+		if(Input.GetButtonDown("Jump"))
+		{
+			rigidBody2D.AddForce (jumpingForce, ForceMode2D.Impulse);
+			//currentState = States.Jumping;
+		}
+	}
+}

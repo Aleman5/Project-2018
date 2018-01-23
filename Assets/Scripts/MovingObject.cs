1e3b7e32c5c0c85e3d52d5a4892a64abb6f595d6
Unknown <ale.bros@hotmail.com>
2018-01-22 23:17:23 -0300

    Player's movement

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -4,42 +4,111 @@ using UnityEngine;
 
 public abstract class MovingObject : MonoBehaviour {
 
-
-	public BoxCollider2D boxCollider2D;
-	public Rigidbody2D rigidBody2D;
 	public int hp;
-	public float speed;
 	public float weight;
 	public float shield;
-	//public float gravity; // Don't know how to implement it.
+	public float minGroundNormalY = .65f;
+	public float gravityModifier = 3f;
 
-	protected int floorMask;
-	protected Vector3 movement;
+	protected BoxCollider2D boxCollider2D;
+	protected Rigidbody2D rigidBody2D;
+	protected SpriteRenderer spriteRenderer;
 	protected Animator animator;
+	protected Vector2 velocity;
+	protected Vector2 targetVelocity;
+	protected Vector2 groundNormal;
+	protected ContactFilter2D contactFilter;
+	protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+	protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
+	protected bool grounded;
 
-	protected virtual void Start () 
+	protected const float minMoveDistance = 0.001f;
+	protected const float shellRadius = 0.01f;
+
+	void OnEnable ()
+	{
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start () 
 	{
 		boxCollider2D = GetComponent <BoxCollider2D> ();
 		rigidBody2D = GetComponent <Rigidbody2D> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		animator = GetComponent<Animator> ();
 		hp = 100;
-		speed = 4f;
 		weight = 0f;
 		shield = 0f;
-		//gravity = 9.8f;
-
-		floorMask = LayerMask.GetMask ("Floor");
-		movement.Set (0, 0, 0);
-		animator = GetComponent<Animator> ();
-	}
-	/*
-	protected virtual void OnCollide <T> ()
-	{
-		
+		//Sets to filter contact results based on trigger collider involvement.
+		contactFilter.useTriggers = false;
+		//Sets the layerMask filter property using the layerMask parameter provided and also 
+		// enables layer mask filtering by setting useLayerMask to true.
+		contactFilter.SetLayerMask (Physics2D.GetLayerCollisionMask (gameObject.layer));
+		//Sets the contact filter to filter results by layer mask.
+		contactFilter.useLayerMask = true;
 	}
 	// Update is called once per frame
 	void Update () 
 	{
+		targetVelocity = Vector2.zero;
+		ComputeVelocity ();
+	}
+	protected virtual void ComputeVelocity()
+	{
 		
 	}
-	*/
+	void FixedUpdate()
+	{
+		velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+		velocity.x = targetVelocity.x;
+
+		grounded = false;
+
+		Vector2 deltaPosition = velocity * Time.deltaTime;
+
+		Vector2 moveAlongGround = new Vector2 (groundNormal.y, -groundNormal.x);
+
+		Vector2 move = moveAlongGround * deltaPosition.x;
+
+		Movement (move, false);
+
+		move = Vector2.up * deltaPosition.y;
+
+		Movement (move, true);
+	}
+	void Movement (Vector2 move, bool yMovement)
+	{
+		float distance = move.magnitude;
+		if (distance > minMoveDistance) 
+		{
+			int count = rigidBody2D.Cast (move, contactFilter, hitBuffer, distance + shellRadius);
+			hitBufferList.Clear ();
+			for (int i = 0; i < count; i++)
+				hitBufferList.Add (hitBuffer [i]);
+			
+			for (int i = 0; i < hitBufferList.Count; i++) 
+			{
+				Vector2 currentNormal = hitBufferList [i].normal;
+				if (currentNormal.y > minGroundNormalY) 
+				{
+					grounded = true;
+					if (yMovement) 
+					{
+						groundNormal = currentNormal;
+						currentNormal.x = 0;
+					}
+				}
+				//Actually, 'currentNormal' has a value on the Y axis.
+				float projection = Vector2.Dot (velocity, currentNormal);
+				if (projection > 0) 
+				{
+					velocity = velocity - projection * currentNormal;
+				}
+
+				float modifiedDistance = hitBufferList [i].distance - shellRadius;
+				distance = modifiedDistance < distance ? modifiedDistance : distance;
+			}
+		}
+		rigidBody2D.position = rigidBody2D.position + move.normalized * distance;
+	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,27 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MovingObject {
-
-	private enum States
+	/*
+	enum States
 	{	
 		Idle,
 		Moving,
 		Jumping,
 		Died
 	};
+	
+	//States currentState;
 
-	private States currentState;
-
-	public Vector2 jumpingForce;
-
-	protected override void Start () 
+	void Awake () 
 	{
 		currentState = States.Idle;
-		jumpingForce.Set (0, 20f);
-		//rigidBody2D.gravityScale = gravity;
-
-		base.Start ();
 	}
+
 	private void FixedUpdate () // It's using "FixedUpdate" because a Rigidbody is being used.
 	{
 		StateMachine ();
@@ -33,20 +28,59 @@ public class Player : MovingObject {
 		switch (currentState) 
 		{
 		case States.Idle:
-			Movement ();
-			Jump ();
 			break;
 		case States.Moving:
-			Movement ();
-			Jump ();
 			break;
 		case States.Jumping:
-			Movement ();
 			break;
 		case States.Died:
 			break;
 		}
 	}
+	*/
+
+	public float maxSpeed = 7;
+	public float jumpTakeOffSpeed = 12;
+
+	protected override void ComputeVelocity()
+	{
+		Vector2 move = Vector2.zero;
+
+		move.x = Input.GetAxis ("Horizontal");
+
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			velocity.y = jumpTakeOffSpeed;
+		} else if (Input.GetButtonUp ("Jump")) 
+		{
+			if (velocity.y > 0)
+				velocity.y = velocity.y * 0.5f;
+		}
+
+		if (move.x > 0.01f) {
+			if (spriteRenderer.flipX)
+				spriteRenderer.flipX = false;
+		}
+		else if (move.x < -0.01f) {
+			if (!spriteRenderer.flipX)
+				spriteRenderer.flipX = true;
+		}
+		animator.SetBool ("grounded", grounded);
+		animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+
+		targetVelocity = move * maxSpeed;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+/*
 	private void Movement()
 	{
 		movement.Set (0, 0, 0);
@@ -65,4 +99,4 @@ public class Player : MovingObject {
 			//currentState = States.Jumping;
 		}
 	}
-}
+*/
